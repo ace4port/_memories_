f5371a4c54347fc85ced76d6d2dd8a5654ff4187
ace4port <39233007+ace4port@users.noreply.github.com>
2021-03-04 15:44:15 +0545

    make it movve

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 app.use('/user', userRouter);
+app.get('./', (req, res) => {
+    res.send('Hello hello');
+});
 
 const PORT = process.env.PORT || 5000;
 
